bf3df1cf4891bd99329639c73cf17605adbc1358
Name <jeremiaheli.lucas@benilde.edu.ph>
2025-06-18 12:04:08 +0000

    Edit settings.py

--- DjangoTest/settings.py
+++ DjangoTest/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'DjangoTest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dockertest',
+        'USER': 'root',
+        'PASSWORD': 'example',
+        'HOST': '192.168.186.128'
     }
 }
 
